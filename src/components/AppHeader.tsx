35b5bc5d752e79bcf347a6ad879d00d09ad72ed9
Christian Demant <cnd@banknorwegian.no>
2025-07-20 12:00:00 +0200

    Center download/upload icons

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
-import Link from "next/link";
 import useExportFunctions from "@/hooks/useExportFunctions";
-import { GitHubIcon, UploadIcon, DownloadIcon, DonateIcon } from "@/components/UI/Icons";
+import { UploadIcon, DownloadIcon } from "@/components/UI/Icons";
 import DropDownButton from "./UI/DropDownButton";
 
 interface AppHeaderProps {
@@ -14,67 +12,47 @@ export default function AppHeader({ fileUploadRef }: AppHeaderProps) {
   const { handleDownloadMd, handleSaveAsSlides } = useExportFunctions();
 
   return (
-    <header className="py-1 px-2  md:py-2 h-16    flex justify-between items-center text-nord9 md:px-4">
-      <Link href="/" className=" px-2 w-full">
-        <Image
-          src="/logo.svg"
-          alt="md Presentation Logo"
-          className="  block md:hidden m-0  h-12  "
-          width={20}
-          height={20}
-          priority
-        />
-        <Image
-          src="/markweavia.svg"
-          alt="md Presentation Text Logo"
-          className=" hidden md:block m-0 h-12   "
-          width={180}
-          height={40}
-          priority
-        />
-      </Link>
-      <div className="flex items-center justify-center space-x-2 w-full ">
-        <div
-          className="flex flex-row items-center group transition-all  px-1 py-1 rounded-md ease-in-out duration-700  w-10 hover:w-32 overflow-hidden"
-          title="Download a Markdown file (.md) or HTML slides"
-        >
-          <DropDownButton
-            color="text-nord9"
-            options={{
-              Slides: "HTML Slides",
-              ".md": "Markdown (.md)",
-            }}
-            onSelect={(option) => {
-              if (option === ".md") {
-                handleDownloadMd();
-              } else {
-                handleSaveAsSlides();
-              }
-            }}
-          >
-            <span className="flex-shrink-0 ">
-              <DownloadIcon />
-            </span>
-            <div className="whitespace-nowrap  opacity-0   group-hover:opacity-100 transition-opacity duration-700">
-              <span className="ml-1.5 whitespace-nowrap  opacity-0   group-hover:opacity-100 transition-opacity duration-700">
-                Download
-              </span>
-            </div>
-          </DropDownButton>
-        </div>
-        <button
-          onClick={triggerFileUpload}
-          className="flex flex-row items-center group transition-all  px-1 py-1 rounded-md ease-in-out duration-700  w-10 hover:w-24 overflow-hidden"
-          title="Upload a Markdown file (.md) (Ctrl+O)"
+    <header className="py-1 px-2 md:py-2 h-16 flex justify-center items-center space-x-2 text-nord9 md:px-4">
+      <div
+        className="flex flex-row items-center group transition-all px-1 py-1 rounded-md ease-in-out duration-700 w-10 hover:w-32 overflow-hidden"
+        title="Download a Markdown file (.md) or HTML slides"
+      >
+        <DropDownButton
+          color="text-nord9"
+          options={{
+            Slides: "HTML Slides",
+            ".md": "Markdown (.md)",
+          }}
+          onSelect={(option) => {
+            if (option === ".md") {
+              handleDownloadMd();
+            } else {
+              handleSaveAsSlides();
+            }
+          }}
         >
           <span className="flex-shrink-0">
-            <UploadIcon />
+            <DownloadIcon />
           </span>
-          <span className="ml-1.5 whitespace-nowrap  opacity-0   group-hover:opacity-100 transition-opacity duration-700">
-            Upload
-          </span>
-        </button>{" "}
+          <div className="whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity duration-700">
+            <span className="ml-1.5 whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity duration-700">
+              Download
+            </span>
+          </div>
+        </DropDownButton>
       </div>
+      <button
+        onClick={triggerFileUpload}
+        className="flex flex-row items-center group transition-all px-1 py-1 rounded-md ease-in-out duration-700 w-10 hover:w-24 overflow-hidden"
+        title="Upload a Markdown file (.md) (Ctrl+O)"
+      >
+        <span className="flex-shrink-0">
+          <UploadIcon />
+        </span>
+        <span className="ml-1.5 whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity duration-700">
+          Upload
+        </span>
+      </button>
     </header>
   );
 }
